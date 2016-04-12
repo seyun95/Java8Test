e9de0cb3cfdc1ae4e71b55cb8e8e39ba55da0bc8
kesan73 <kesan73@gmail.com>
2016-04-12 11:12:01 +0900

    kesan Test

--- Java8Test/src/SortTest.java
+++ Java8Test/src/SortTest.java
@@ -15,6 +15,7 @@ public class SortTest {
 		Arrays.sort(files, new Comparator<File>(){
 		    public int compare(File f1, File f2)
 		    {
+		    	int n = 1;
 		    	return String.valueOf(f1.getName()).compareTo(f2.getName());
 		        //return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
 		    } });
